bda3c7c5050951e245dbb6b3f26de4e1155efbe2
brandonchaffee <brandon@patterncapture.com>
2018-06-28 13:15:17 -0700

    readded vue build files

--- src/util/getContract.js
+++ src/util/getContract.js
@@ -1,11 +1,13 @@
 import Web3 from 'web3'
 import {address, ABI} from './constants/casinoContract'
-import Expopulo from '../../build/contracts/Expopulo.json'
+import expopulo from './generated/expopulo.json'
+
 let getContract = new Promise(function (resolve, reject) {
   let web3 = new Web3(window.web3.currentProvider)
-  let expopuloContract = web3.eth.contract(Expopulo.abi)
+  let expopuloContract = web3.eth.contract(expopulo.abi)
   let casinoContract = web3.eth.contract(ABI)
   let casinoContractInstance = casinoContract.at(address)
+  console.log('Expop add', expopulo.address)
   console.log('Expop contract', expopuloContract)
   console.log(casinoContractInstance)
   resolve(casinoContractInstance)
